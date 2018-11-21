34c47459ba4761959c9e16658404821188680b53
classroom <classroom@feckc.org>
2018-11-21 11:17:40 -0600

    my first javaScript

---
+++ practice2.js
@@ -0,0 +1,12 @@
+function  dateAndTime(){
+    document.getElementById('dateAndTime').innerHTML = Date();
+}
+function conosolePrint(){
+    console.log("hello Steve");
+    }
+    function alertPrint(){
+    alert("hello Steve");
+    }
+    function documentPrint(){
+    document.write("hello Steve")
+    }
